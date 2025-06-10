4a3a9b3a0f1de541711c8344db279c3378f06f53
PotmanNob <Potmannob@outlook.com>
2025-06-10 18:19:37 -0500

    lalalalal

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        // create an array of ints
+        int[] nums = {1, 2, 3, 4, 5};
+
+        // creates array of size 4
+        int[] nums2 = new int[2];
+        // assigns values to the array
+        nums2[0] = 10;
+        nums2[1] = 20;
+
+        // print the first element of the array
+        System.out.println(nums[0]); //output: 1
+        // print out array length
+        System.out.println(nums.length); //output: 5
+        for (int i = 0; i < nums.length; i++) {
+            //loops through an array
+        }
+    }
+}
